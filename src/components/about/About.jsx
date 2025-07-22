9bd1e3f417ec9bd888cb8a37415e7f04807cf9d8
Nabeel-KM <nabeel.sarfraz@kryptomind.com>
2025-07-22 10:02:37 +0500

    Changed Image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./about.css";
-import ME from "../../assets/me-about.jpg";
+import ME from "../../assets/me-about.png";
 import { FaAward, FaCloudUploadAlt, FaTools, FaShieldAlt, FaSearchDollar, FaLinkedin, FaGithub, FaEnvelope, FaPhone, FaMapMarkerAlt, FaChevronDown } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
